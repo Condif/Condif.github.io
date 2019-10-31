0cb30e4271119e90a92dc117fc26496be4d59ebf
Condif <filip.sunnemar@medieinstitutet.se>
2019-10-31 18:02:06 +0100

    Added a new for answer2 in path 1

--- java.js
+++ java.js
@@ -1,4 +1,8 @@
 function start() {
     alert("Test")
     var storeAnswer1 = prompt("So what do you want to do? \n Attack \n Run \ Hide \n Type in your answer using the exact word you would like to choose")
+    if (storeAnswer1 === "Attack") {
+        alert("You attack with the devils fury but launches straight through your opponent")
+        let storeAnswer2 = prompt("Confused by the recent line of events Lucifer \n stumbles and looks behind him, but there is no longer a foe to be seen.\nLucifer most continue his path to find the flaming sword an avenge his father \n")
+    }
 }
