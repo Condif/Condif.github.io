b8fb0e094fa17adb19644a13ffbf023e46478e14
Condif <filip.sunnemar@medieinstitutet.se>
2019-10-31 17:53:37 +0100

    change to backslash n

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
 function start() {
     alert("Test")
-    var storeAnswer1 = prompt("So what do you want to do? /n Attack /n Run /n Hide /n Type in your answer using the exact word you would like to choose")
+    var storeAnswer1 = prompt("So what do you want to do? \n Attack \n Run \ Hide \n Type in your answer using the exact word you would like to choose")
 }
